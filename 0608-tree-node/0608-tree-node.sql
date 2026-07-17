ac5f080d5194a2e5aa10d0c09316a7ca3f2381d6
Sumit verma <130686003+Sumitverma0@users.noreply.github.com>
2026-07-17 15:09:03 +0530

    Sync solution for: Tree Node

---
+++ 0608-tree-node/0608-tree-node.sql
@@ -0,0 +1,23 @@
+s
+{"headers":{"Tree":["id","p_id"]},"rows":{"Tree":[[1,null],[2,1],[3,1],[4,2],[5,2]]}}
+{"headers":{"Tree":["id","p_id"]},"rows":{"Tree":[[1,null]]}}
+| id | type  |
+| -- | ----- |
+| 1  | Root  |
+| 2  | Inner |
+| 3  | Leaf  |
+| 4  | Leaf  |
+| 5  | Leaf  |
+| id | type |
+| -- | ---- |
+| 1  | Root |
+| id | type  |
+| -- | ----- |
+| 1  | Root  |
+| 2  | Inner |
+| 3  | Leaf  |
+| 4  | Leaf  |
+| 5  | Leaf  |
+| id | type |
+| -- | ---- |
+| 1  | Root |
